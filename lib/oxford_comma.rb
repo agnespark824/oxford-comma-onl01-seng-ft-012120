293ed98df24a757bb5f2f53d2d93d616e5698cd3
Agnes Park <agnespark824@gmail.com>
2020-01-22 21:51:56 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,9 @@
 def oxford_comma(array)
   if array.length <= 1
-    
+    array[0]
+  elsif array.length ==2 
+    array.join(" and ") + "."
+  else
+    last_element = array.pop
+    array.join(", ") + "and #{last_element}."
 end
